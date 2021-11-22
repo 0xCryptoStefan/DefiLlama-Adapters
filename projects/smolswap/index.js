3b62212658ee068ff27ed5e6ea37dba621f8b805
0xStefan <blockchain.crypto.stefan@gmail.com>
2021-11-22 16:43:17 +0000

    added smolswap

--- projects/smolswap/index.js
+++ projects/smolswap/index.js
@@ -2,6 +2,6 @@ const {calculateUsdUniTvl} = require('../helper/getUsdUniTvl')
 
 module.exports={
     cronos:{
-        tvl: calculateUsdUniTvl("0x7Aa2149fF9EF4A09D4ace72C49C26AaE8C89Fb48", "cronos", "0x2Ad63Da83d6ff5dA9E716DcaE844D4f157405BDd", [], "ethereum")
+        tvl: calculateUsdUniTvl("0x7Aa2149fF9EF4A09D4ace72C49C26AaE8C89Fb48", "cronos", "0x5C7F8A570d578ED84E63fdFA7b1eE72dEae1AE23", [], "crypto-com-chain")
     }
 }
